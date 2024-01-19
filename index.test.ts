294824597b5ef0871bec81cb039aa10047ccca7b
The24thDS <hello@david-sima.dev>
2024-01-19 20:38:06 +0200

    fix: fixed test failing after upload because the banner doesn't go away

--- index.test.ts
+++ index.test.ts
@@ -25,6 +25,7 @@ const RELEASE_NOTES = process.env.RELEASE_NOTES || 'No release notes provided.';
 
 test('Upload mod', async ({ page }) => {
 	// Step 1. Login
+	console.info('Step 1. Login');
 	await page.goto(
 		'https://login.paradoxplaza.com/login?service=https%3A%2F%2Fmods.paradoxplaza.com%2Fvalidate%3Fredirect%3D%252F'
 	);
@@ -36,6 +37,7 @@ test('Upload mod', async ({ page }) => {
 	await expect(page.getByText(/validating your login/i)).toHaveCount(0);
 
 	// Step 2. Open mod form
+	console.info('Step 2. Open mod form');
 	await page.goto(`https://mods.paradoxplaza.com/mods/${MOD_ID}/Any`);
 	await page.getByRole('link', { name: 'Edit / New version' }).click();
 	await expect(
@@ -45,11 +47,13 @@ test('Upload mod', async ({ page }) => {
 	await page.waitForTimeout(500);
 
 	// Step 3. Fill version tab
+	console.info('Step 3. Fill version tab');
 	await page.getByRole('tab', { name: 'version' }).click();
 	await page.locator('input[name="userModVersion"]').fill(MOD_VERSION);
 	await page.locator('input[name="version"]').fill(GAME_VERSION);
 
 	// Step 4. Upload zip file
+	console.info('Step 4. Upload zip file');
 	await page.getByRole('tab', { name: 'files' }).click();
 	const fileChooserPromise = page.waitForEvent('filechooser');
 	await page.getByLabel('files').locator('div[role="button"]').click();
@@ -60,6 +64,7 @@ test('Upload mod', async ({ page }) => {
 	).not.toHaveClass(/__active/, { timeout: UPLOAD_TIMEOUT });
 
 	// Step 5. Publish new version
+	console.info('Step 5. Publish new version');
 	await page.getByRole('tab', { name: 'preview' }).click();
 	await page
 		.getByRole('button', { name: /publish mod/i })
@@ -67,7 +72,6 @@ test('Upload mod', async ({ page }) => {
 		.click();
 	await page.getByRole('textbox').fill(RELEASE_NOTES);
 	await page.getByRole('button', { name: 'Submit to changelog' }).click();
-	await expect(
-		page.getByText(/your mod is currently being published/i)
-	).toHaveCount(1);
+	await page.waitForURL(/.*\/uploaded\?.*/);
+	console.info('Mod uploaded!');
 });
